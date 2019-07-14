5162b534e593fb9c678e4d53e8ad037559c78765
Elliot Clark <etcclark@gmail.com>
2019-07-14 15:35:10 -0700

    Header Section Finished

--- public/js/Trianglify.js
+++ public/js/Trianglify.js
@@ -1,14 +1,29 @@
 let trianglifyHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
 
+/* setInterval(function(){ 
+        var pattern = Trianglify({
+            cell_size: 1100,
+            x_colors: ['#000000', '#4CAFE8', '#FFFFFF'],
+            y_colors: 'match_x',
+            stroke_width: 51.51,
+            seed: 'byttg',
+            width: window.innerWidth,
+            height: trianglifyHeight
+        });
+        document.body.appendChild(pattern.canvas());
+}, 3000); */
+
+
 var pattern = Trianglify({
-    cell_size: 100,
+    cell_size: 40,
     x_colors: ['#000000', '#4CAFE8', '#FFFFFF'],
     y_colors: 'match_x',
     stroke_width: 51.51,
     seed: 'byttg',
     width: window.innerWidth,
-    height: trianglifyHeight
+    height: window.innerHeight
 });
 document.body.appendChild(pattern.canvas());
+
 
     
