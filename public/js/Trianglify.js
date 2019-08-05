008038e01c4be74b57df0ebc507e7c57fd24b6f1
Elliot Clark <etcclark@gmail.com>
2019-08-04 22:03:38 -0700

    General adjustments on all sections

--- public/js/Trianglify.js
+++ public/js/Trianglify.js
@@ -1,13 +1,15 @@
 'use strict'
 
+//#F2EDE2
+let randomCellSize = Math.random() * (150 - 80) + 80;
 var pattern = Trianglify({
-    cell_size: 50,
-    x_colors: ['#000000', '#4CAFE8', '#FFFFFF'],
-    y_colors: 'random',
-    stroke_width: 2.51,
+    cell_size: randomCellSize,
+    y_colors: ['#4CAFE8', '#A4D3E2', '#F4EEE1'],
+    x_colors: ['#F4EEE1', '#F4EEE1'],
+    stroke_width: 15,
     seed: 'null',
     width: window.innerWidth,
-    height: window.innerHeight + 1
+    height: window.innerHeight
 });
 var trianglifyBackground = document.getElementById("trianglifyBackground");
 trianglifyBackground.appendChild(pattern.canvas());
--- public/js/app.js
+++ public/js/app.js
@@ -6,4 +6,24 @@
 //         ele[x].classList.add('header-transition');
 //     }
 //     document.getElementById("profile").style.display = "block";
+//     document.getElementById("experiences").style.display = "block";
+//     document.getElementById("projects").style.display = "block";
 //   });
+
+
+let arr = ["Hi, I'm Elliot Clark. I'm a Front End Web Developer!"];
+let arrLetters = arr[0].split('');
+
+let logo = [];
+const logoAnimation = () => {
+    logo.push(arrLetters[0]);
+    arrLetters.shift();
+    document.getElementById('logo').innerHTML = logo.join("");
+    if (arrLetters.length > 0) {
+        setTimeout(logoAnimation, 42); 
+    }
+}
+
+setTimeout(function(){
+    logoAnimation();
+ }, 3500);
