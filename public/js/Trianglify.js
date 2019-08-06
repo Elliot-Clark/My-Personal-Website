88751649ec6b9a56226c3256f7139c351ecb8fdf
Elliot Clark <etcclark@gmail.com>
2019-08-06 14:42:10 -0700

    Added screen resize rerender

--- public/js/Trianglify.js
+++ public/js/Trianglify.js
@@ -1,15 +1,22 @@
 'use strict'
 
 //#F2EDE2
-let randomCellSize = Math.random() * (150 - 80) + 80;
-var pattern = Trianglify({
-    cell_size: randomCellSize,
-    y_colors: ['#4CAFE8', '#A4D3E2', '#F4EEE1'],
-    x_colors: ['#F4EEE1', '#F4EEE1'],
-    stroke_width: 15,
-    seed: 'null',
-    width: window.innerWidth,
-    height: window.innerHeight
-});
-var trianglifyBackground = document.getElementById("trianglifyBackground");
-trianglifyBackground.appendChild(pattern.canvas());
+
+const render = () => {
+    if (document.getElementsByTagName("canvas")[0]) {
+        document.getElementsByTagName("canvas")[0].remove();
+    }
+    let pattern = Trianglify({
+        cell_size: Math.random() * (150 - 80) + 80,
+        y_colors: ['#4CAFE8', '#A4D3E2', '#F4EEE1'],
+        x_colors: ['#F4EEE1', '#F4EEE1'],
+        stroke_width: Math.random() * (17 - 5) + 5,
+        seed: 'null',
+        width: window.innerWidth,
+        height: window.innerHeight
+    });
+    const trianglifyBackground = document.getElementById("trianglifyBackground");
+    trianglifyBackground.appendChild(pattern.canvas());
+}
+window.onload = render;
+window.onresize = render; 
