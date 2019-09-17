0d6337b11e1e2b833bca440ce9eb5a586b0826e6
Elliot Clark <etcclark@gmail.com>
2019-09-16 22:28:50 -0700

    Profile Animation Adjustment

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,5 @@
 //'use strict'
 
-
-
-//Logo Animation
-let arr = ["Hi, I'm Elliot Clark. I'm a Front End Web Developer!"];
-let arrLetters = arr[0].split('');
-let logo = [];
-const logoAnimation = () => {
-    logo.push(arrLetters[0]);
-    arrLetters.shift();
-    document.getElementById('logo').innerHTML = logo.join("");
-    if (arrLetters.length > 0) {
-        setTimeout(logoAnimation, 42); 
-    }
-}
-setTimeout(function(){
-    logoAnimation();
- }, 3500);
-
-
  //Code for Lazy Loading all images
  const targets = document.querySelectorAll('img');
  const lazyLoad = target => {
